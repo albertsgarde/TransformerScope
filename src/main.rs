2b28f30dcb0246696fa8369d4630546702b013d4
Albert Garde <albertsgarde@gmail.com>
2023-04-28 15:57:11 +0200

    :sparkles: Remove last traces of initial actix exploration

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
 use std::env;
 
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpServer, Responder};
 use maud::html;
 use ndarray::s;
 use transformer_scope::{board_heatmap, ApplicationState};
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    println!("helleflynder");
-    HttpResponse::Ok().body("Hello world!")
+async fn index() -> impl Responder {
+    html! {
+        head {
+            meta charset="utf-8";
+            title { "Transformer Scope" }
+        }
+        body {
+            h1 { "Transformer Scope" }
+            p { "This is a web app for visualizing various aspects of transformer models." }
+            p { "This page should include an index, but this has not been implemented yet." }
+        }
+    }
 }
 
 #[get("/L{layer_index}/N{neuron_index}")]
-async fn echo(
+async fn neuron(
     data: web::Data<ApplicationState>,
     path: web::Path<(usize, usize)>,
 ) -> impl Responder {
@@ -34,10 +43,6 @@ async fn echo(
     }
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let path = env::args().nth(1).unwrap();
@@ -47,9 +52,8 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(state.clone()))
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .service(index)
+            .service(neuron)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
