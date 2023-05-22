6cf231d690edf5cf848553e20d268afa560ddeda
Albert Garde <albertsgarde@gmail.com>
2023-05-22 11:35:56 +0200

    :bug: Handle keyboard interrupts in Rust parts of Python package"

--- transformer-scope/python/transformer_scope/__init__.py
+++ transformer-scope/python/transformer_scope/__init__.py
@@ -1,4 +1,6 @@
 #from .transformer_scope import PayloadBuilder, Payload
 
 from .payload import PayloadBuilder, Payload
-from .transformer_scope import Scope
+from .transformer_scope import Scope, setup_keyboard_interrupt
+
+setup_keyboard_interrupt()
