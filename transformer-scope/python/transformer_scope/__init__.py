8489fffc2fd636b75990d879da3829dc8715866b
Albert Garde <albertsgarde@gmail.com>
2023-05-28 23:25:42 +0200

    :bug: Fix reexport typo

--- transformer-scope/python/transformer_scope/__init__.py
+++ transformer-scope/python/transformer_scope/__init__.py
@@ -1,6 +1,6 @@
 # from .transformer_scope import PayloadBuilder, Payload
 
-from .logit_attribution import logit_attributions
+from .logit_attribution import mlp_logit_attributions
 from .payload import Payload, PayloadBuilder
 from .transformer_scope import Scope, setup_keyboard_interrupt
 
